d604d3fafc9133b13dcbde4b79f7a2992d25ac73
Siddhant Patil <sidomatic11@gmail.com>
2024-02-15 18:22:34 +0530

    Face landmarking implemented

--- src/script.js
+++ src/script.js
@@ -1,10 +1,15 @@
-// TODO: Verify image recognition calculation. Why is it not correct?
-
-import { FilesetResolver, FaceDetector } from "@mediapipe/tasks-vision";
+import {
+	FilesetResolver,
+	FaceDetector,
+	FaceLandmarker,
+	DrawingUtils,
+} from "@mediapipe/tasks-vision";
 import blazeModelPath from "/models/blaze_face_short_range.tflite?url";
+import landmarkerModelPath from "/models/face_landmarker.task?url";
 
 let runningMode = "IMAGE";
 let faceDetector;
+let faceLandmarker;
 
 /* SECTION: INITIALIZE DETECTOR */
 
@@ -21,9 +26,27 @@ async function initializeFaceDetector() {
 	});
 }
 
+async function initializeFaceLandmarker() {
+	const vision = await FilesetResolver.forVisionTasks(
+		"https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@latest/wasm"
+	);
+
+	faceLandmarker = await FaceLandmarker.createFromOptions(vision, {
+		baseOptions: {
+			modelAssetPath: landmarkerModelPath,
+			delegate: "GPU",
+		},
+		outputFaceBlendshapes: true,
+		runningMode,
+		numFaces: 1,
+	});
+}
+
 initializeFaceDetector().then(() => {
 	document.getElementById("loading").remove();
 });
+
+initializeFaceLandmarker();
 
 /* SECTION: IMAGE DETECTION */
 
@@ -35,7 +58,7 @@ imageInput.addEventListener("change", function () {
 	const reader = new FileReader();
 	reader.onload = function (e) {
 		imageTag.src = e.target.result;
-		imageTag.onload = detectInImage;
+		imageTag.onload = detectLandmarksInImage;
 	};
 	reader.readAsDataURL(file);
 });
@@ -98,6 +121,73 @@ function displayImageDetections(detection) {
 	this.displayContainer.appendChild(boundinBoxElement);
 }
 
+function detectLandmarksInImage() {
+	const image = document.getElementById("image1");
+	const faceLandmarkerResult = faceLandmarker.detect(image);
+	console.log(faceLandmarkerResult);
+
+	let displayContainer = document.getElementById("detections-container");
+	displayContainer.innerHTML = "";
+
+	const canvas = document.createElement("canvas");
+	canvas.setAttribute("class", "canvas");
+
+	/* Canvas size = Actual image size, to match resolution */
+	canvas.setAttribute("width", image.naturalWidth + "px");
+	canvas.setAttribute("height", image.naturalHeight + "px");
+
+	displayContainer.appendChild(canvas);
+
+	/* Draw on canvas using DrawingUtils */
+	const ctx = canvas.getContext("2d");
+	const drawingUtils = new DrawingUtils(ctx);
+	for (const landmarks of faceLandmarkerResult.faceLandmarks) {
+		drawingUtils.drawConnectors(
+			landmarks,
+			FaceLandmarker.FACE_LANDMARKS_TESSELATION,
+			{ color: "#C0C0C070", lineWidth: 1 }
+		);
+		drawingUtils.drawConnectors(
+			landmarks,
+			FaceLandmarker.FACE_LANDMARKS_RIGHT_EYE,
+			{ color: "#FF3030" }
+		);
+		drawingUtils.drawConnectors(
+			landmarks,
+			FaceLandmarker.FACE_LANDMARKS_RIGHT_EYEBROW,
+			{ color: "#FF3030" }
+		);
+		drawingUtils.drawConnectors(
+			landmarks,
+			FaceLandmarker.FACE_LANDMARKS_LEFT_EYE,
+			{ color: "#30FF30" }
+		);
+		drawingUtils.drawConnectors(
+			landmarks,
+			FaceLandmarker.FACE_LANDMARKS_LEFT_EYEBROW,
+			{ color: "#30FF30" }
+		);
+		drawingUtils.drawConnectors(
+			landmarks,
+			FaceLandmarker.FACE_LANDMARKS_FACE_OVAL,
+			{ color: "#E0E0E0" }
+		);
+		drawingUtils.drawConnectors(landmarks, FaceLandmarker.FACE_LANDMARKS_LIPS, {
+			color: "#E0E0E0",
+		});
+		drawingUtils.drawConnectors(
+			landmarks,
+			FaceLandmarker.FACE_LANDMARKS_RIGHT_IRIS,
+			{ color: "#FF3030" }
+		);
+		drawingUtils.drawConnectors(
+			landmarks,
+			FaceLandmarker.FACE_LANDMARKS_LEFT_IRIS,
+			{ color: "#30FF30" }
+		);
+	}
+}
+
 /* SECTION: VIDEO DETECTION */
 
 let video = document.getElementById("webcam");
@@ -110,7 +200,8 @@ const hasGetUserMedia = () => !!navigator.mediaDevices?.getUserMedia;
 
 if (hasGetUserMedia()) {
 	enableWebcamButton = document.getElementById("webcamButton");
-	enableWebcamButton.addEventListener("click", enableCam);
+	// enableWebcamButton.addEventListener("click", enableCam);
+	enableWebcamButton.addEventListener("click", enableCamForLandmarker);
 } else {
 	console.warn("getUserMedia() is not supported by your browser");
 }
@@ -212,4 +303,112 @@ function displayVideoDetections(detection) {
 		liveView.appendChild(keypointElement);
 		children.push(keypointElement);
 	});
+}
+
+let displayContainer = document.getElementById("video-detections-container");
+const canvas = document.createElement("canvas");
+canvas.setAttribute("class", "canvas");
+displayContainer.appendChild(canvas);
+
+async function enableCamForLandmarker(event) {
+	// Hide button
+	enableWebcamButton.style.display = "none";
+
+	// Set parameters for getUsermedia
+	const constraints = {
+		video: true,
+	};
+
+	// Activate the webcam stream.
+	navigator.mediaDevices
+		.getUserMedia(constraints)
+		.then(function (stream) {
+			video.srcObject = stream;
+			video.addEventListener("loadeddata", detectLandmarksInWebcam);
+		})
+		.catch((err) => {
+			console.error(err);
+		});
+}
+
+async function detectLandmarksInWebcam() {
+	// if image mode is initialized, create a new classifier with video runningMode
+	if (runningMode === "IMAGE") {
+		runningMode = "VIDEO";
+		await faceLandmarker.setOptions({ runningMode: "VIDEO" });
+		/* Canvas size = Actual image size, to match resolution */
+		canvas.setAttribute("width", video.videoWidth + "px");
+		canvas.setAttribute("height", video.videoHeight + "px");
+	}
+
+	let startTimeMs = performance.now();
+
+	// displayContainer.appendChild(canvas);
+
+	// Detect faces using detectForVideo
+	if (video.currentTime !== lastVideoTime) {
+		lastVideoTime = video.currentTime;
+
+		const detections = faceLandmarker.detectForVideo(video, startTimeMs);
+
+		if (detections.faceLandmarks) {
+			/* Draw on canvas using DrawingUtils */
+			const ctx = canvas.getContext("2d");
+			ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height); //clear canvas before redrawing
+			const drawingUtils = new DrawingUtils(ctx);
+
+			for (const landmarks of detections.faceLandmarks) {
+				drawingUtils.drawConnectors(
+					landmarks,
+					FaceLandmarker.FACE_LANDMARKS_TESSELATION,
+					{ color: "#C0C0C070", lineWidth: 1 }
+				);
+				drawingUtils.drawConnectors(
+					landmarks,
+					FaceLandmarker.FACE_LANDMARKS_RIGHT_EYE,
+					{ color: "#FF3030" }
+				);
+				drawingUtils.drawConnectors(
+					landmarks,
+					FaceLandmarker.FACE_LANDMARKS_RIGHT_EYEBROW,
+					{ color: "#FF3030" }
+				);
+				drawingUtils.drawConnectors(
+					landmarks,
+					FaceLandmarker.FACE_LANDMARKS_LEFT_EYE,
+					{ color: "#30FF30" }
+				);
+				drawingUtils.drawConnectors(
+					landmarks,
+					FaceLandmarker.FACE_LANDMARKS_LEFT_EYEBROW,
+					{ color: "#30FF30" }
+				);
+				drawingUtils.drawConnectors(
+					landmarks,
+					FaceLandmarker.FACE_LANDMARKS_FACE_OVAL,
+					{ color: "#E0E0E0" }
+				);
+				drawingUtils.drawConnectors(
+					landmarks,
+					FaceLandmarker.FACE_LANDMARKS_LIPS,
+					{
+						color: "#E0E0E0",
+					}
+				);
+				drawingUtils.drawConnectors(
+					landmarks,
+					FaceLandmarker.FACE_LANDMARKS_RIGHT_IRIS,
+					{ color: "#FF3030" }
+				);
+				drawingUtils.drawConnectors(
+					landmarks,
+					FaceLandmarker.FACE_LANDMARKS_LEFT_IRIS,
+					{ color: "#30FF30" }
+				);
+			}
+		}
+	}
+
+	// Call this function again to keep predicting when the browser is ready
+	window.requestAnimationFrame(detectLandmarksInWebcam);
 }
